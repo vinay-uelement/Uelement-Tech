0dae4d6efb78585a9c42163faa0694f61db08ea7
U-Element Technologies Private Limited <u-elementtechnologiesprivatelimited@UE-PUN-MB-112025-05.local>
2025-11-19 16:30:48 +0530

    initial project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,18 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  // Add Prettier plugin - must be last to override other configs
+  eslintPluginPrettierRecommended,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
