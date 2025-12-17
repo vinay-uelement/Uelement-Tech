5980b2c18e8a4616e3c269f9211656c2147a52bc
rugwed18 <rugwed@u-element.in>
2025-12-17 15:58:35 +0530

    92 logo opacity changed to 100

--- components/layout/NavbarV4.jsx
+++ components/layout/NavbarV4.jsx
@@ -195,7 +195,7 @@ const NavbarV4 = () => {
               <div className="w-[calc(100%+30px)] md:w-[calc(100%+40px)] h-full bg-[#32323259] backdrop-blur-lg rounded-[40px] -ml-[30px] md:-ml-10 px-3 flex items-center relative z-50">
                 {/* 92 Badge - Always visible */}
                 <div className="size-8 md:size-10 bg-[#9B7025] rounded-full flex-shrink-0 relative">
-                  <span className="absolute -translate-x-1/2 left-1/2 -translate-y-1/2 top-1/2 font-bold text-18 md:text-22 font-reddit-sans text-[#ffffff90]">
+                  <span className="absolute -translate-x-1/2 left-1/2 -translate-y-1/2 top-1/2 font-bold text-18 md:text-22 font-reddit-sans text-[#ffffff]">
                     92
                   </span>
                 </div>
